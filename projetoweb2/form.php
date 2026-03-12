2854171278f4fb8f7cae8f82bf6d573c47ab22bf
Nikolas <131282424+N1k0145@users.noreply.github.com>
2026-03-12 17:39:05 -0300

    Add files via upload

---
+++ projetoweb2/form.php
@@ -0,0 +1,12 @@
+<html>
+    <head>
+        <title>Documento</title>
+    </head>
+    <body>
+        Oi
+        <? echo $_PODT["nome"];?>
+        . Você tem
+        <? echo $_PODT["nome"];?>
+        anos
+    </body>
+<html>
